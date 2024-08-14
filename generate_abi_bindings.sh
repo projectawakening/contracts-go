889d0a0da65a0bb692374a37509e9a236b3e5292
Yann Vaillant <yann@vaillant.im>
2024-08-14 14:39:49 +0000

    chore: fetch ERC20 ABI from S3
    ERC20 is now stored with all the other ABIs that we are using

--- generate_abi_bindings.sh
+++ generate_abi_bindings.sh
@@ -4,6 +4,7 @@ set -o nounset
 worldAbiTemp=$(mktemp)
 ERC2771WorldAbiTemp=$(mktemp)
 ERC2771ForwarderAbiTemp=$(mktemp)
+ERC20AbiTemp=$(mktemp)
 
 # this is the ABI with the prefixed functions so we don't need to use system id
 curl -s https://abi-export.projectawakening.io/IWorld-${VERSION}.abi.json > ${worldAbiTemp}
@@ -13,9 +14,9 @@ curl -s https://abi-export.projectawakening.io/ERC2771IWorld-${VERSION}.abi.json
 
 curl -s https://abi-export.projectawakening.io/ERC2771Forwarder-${VERSION}.abi.json > ${ERC2771ForwarderAbiTemp}
 
+curl -s https://abi-export.projectawakening.io/IERC20-${VERSION}.abi.json > ${ERC20AbiTemp}
+
 abigen --abi=${worldAbiTemp} --pkg=world --out=bindings/world/world.go
 abigen --abi=${ERC2771WorldAbiTemp} --pkg=ERC2771World --out=bindings/ERC2771World/ERC2771World.go
 abigen --abi=${ERC2771ForwarderAbiTemp} --pkg=ERC2771Forwarder --out=bindings/ERC2771Forwarder/ERC2771Forwarder.go
-
-# TODO: host ERC20 abi on abi-export.projectawakening.io
-abigen --abi=abi/ERC20Proxy.abi.json --pkg=ERC20 --out=bindings/ERC20/ERC20.go
+abigen --abi=${ERC20AbiTemp} --pkg=ERC20 --out=bindings/ERC20/ERC20.go
